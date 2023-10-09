3a7e45df7c96de0c9f43664b37d39a14ca2c1ac0
oomori-you <valsamina@gmail.com>
2023-10-10 00:51:16 +0900

    feat: ヘッダーをtextviewにフッターをinputfieldに変更

--- cmd/cd.go
+++ cmd/cd.go
@@ -4,6 +4,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 )
@@ -14,12 +17,23 @@ var cdCmd = &cobra.Command{
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
 		app := tview.NewApplication()
+
+		wdPath, _ := os.Getwd()
+		textview := tview.NewTextView()
+		textview.SetText(wdPath).SetTitle("Top").SetBorder(true)
+
+		inputField := tview.NewInputField()
+		inputField.SetDoneFunc(func(key tcell.Key) {
+			app.Stop()
+		})
+
 		flex := tview.NewFlex().
 			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-				AddItem(tview.NewBox().SetBorder(true).SetTitle("Top - Fixed - 1"), 3, 0, false).
+				//AddItem(textview.SetBorder(true).SetTitle("Top - Fixed - 1"), 30, 0, false).
+				AddItem(textview, 3, 0, false).
 				AddItem(tview.NewBox().SetBorder(true).SetTitle("Middle Proportion"), 0, 1, false).
-				AddItem(tview.NewBox().SetBorder(true).SetTitle("Bottom Fixed - 1"), 3, 0, false),
-				0, 1, false)
+				AddItem(inputField, 1, 0, true),
+				0, 1, true)
 
 		if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 			panic(err)
