f5af3efede93689de3a728be75d76f843b62c5b3
oomori-you <valsamina@gmail.com>
2023-10-10 00:23:44 +0900

    feat: ヘッダー、フッダー、ボディのレイアウトを追加

--- cmd/cd.go
+++ cmd/cd.go
@@ -1,12 +1,10 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 )
 
@@ -14,27 +12,21 @@ import (
 var cdCmd = &cobra.Command{
 	Use:   "cd",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cd called")
+		app := tview.NewApplication()
+		flex := tview.NewFlex().
+			AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+				AddItem(tview.NewBox().SetBorder(true).SetTitle("Top - Fixed - 1"), 3, 0, false).
+				AddItem(tview.NewBox().SetBorder(true).SetTitle("Middle Proportion"), 0, 1, false).
+				AddItem(tview.NewBox().SetBorder(true).SetTitle("Bottom Fixed - 1"), 3, 0, false),
+				0, 1, false)
+
+		if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+			panic(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(cdCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cdCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cdCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,17 @@ go 1.21.1
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rivo/tview v0.0.0-20231007183732-6c844bdc5f7a // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -22,6 +28,7 @@ require (
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
